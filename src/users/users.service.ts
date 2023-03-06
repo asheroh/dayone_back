8acec1c7739df4614621a68366a3b22cf44cd72f
SEONGJOON KIM <jobcho6320@naver.com>
2023-03-06 22:48:21 +0900

    chore: gitignore 수정

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class UsersService {
-  createUser(): string {
-    return 123;
-  }
+  //   createUser(): string {
+  //     return
+  //   }
 }
