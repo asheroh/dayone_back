2875f955149de7e474f512049f080dc0f9b35858
SEONGJOON KIM <jobcho6320@naver.com>
2023-03-30 23:14:21 +0900

    refactor: post route

--- src/routes/post.route.ts
+++ src/routes/post.route.ts
@@ -5,9 +5,9 @@ import auth from '../middlewares/auth';
 const router: Router = express.Router();
 
 router.post('/', auth.validateToken, postController.dayPosting); // 데이기록 작성
+router.get('/', auth.validateToken, postController.getAllPosts); // 전체 유저들의 기록 가져오기
 router.get('/search/', auth.validateToken, postController.getBookTitle); // 네이버 책 검색 API 리다이렉트 URI
 router.get('/', auth.validateToken, postController.getBookTitle); // 네이버 책 API
 router.get('/:userId', auth.validateToken, postController.getUserPosts); // 해당 유저의 기록 가져오기
-router.get('/', auth.validateToken, postController.getAllPosts); // 전체 유저들의 기록 가져오기
 
 export default { router };
