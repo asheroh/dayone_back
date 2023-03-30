dcb39c74aebc3dbad7f2ae1f3b3b71005ab96e28
SEONGJOON KIM <jobcho6320@naver.com>
2023-03-30 23:01:40 +0900

    refactor: kakao login redirect routes

--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -6,7 +6,7 @@ const router: Router = express.Router();
 
 router.get('/info', auth.validateToken, authController.getAllUsers); // 모든 유저의 데이터 가져오기
 router.get('/kakao/start', authController.kakaoLoginStart); // 카카오 로그인 버튼 눌렀을 때, 리다이렉트 URI
-router.get('/users/kakao/finish', authController.kakaoRedirect);
+router.get('/kakao/finish', authController.kakaoRedirect);
 router.get('/api/v1/auth/login', authController.kakaoSignin); // 카카오 로그인
 
 export default { router };
