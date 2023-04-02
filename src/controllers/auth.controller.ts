c7fa5e702410850c175a7376448e3883094f16e9
SEONGJOON KIM <jobcho6320@naver.com>
2023-04-03 00:17:03 +0900

    fix: modify kakao authorization code type

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -5,20 +5,20 @@ class auth {
   async kakaoLoginStart(req: Request, res: Response) {
     const redirectUrl = authService.getKakaoLoginUrl();
     console.log(redirectUrl);
-    return res.redirect(redirectUrl);
+    // const redirect =
+    res.send(redirectUrl);
+    // const token = await this.kakaoRedirect(req, res);
+    // res.status(200).json({ JWT: token });
   }
 
   async kakaoRedirect(req: Request, res: Response) {
-    const kakaoAccessToken = await authService.getKakaoAccessToken(
-      req.query.code as string,
-    );
-
-    const kakaoToken: any = kakaoAccessToken.access_token;
-
+    // const kakaoAccessToken = await authService.getKakaoAccessToken(
+    //   req.headers.authorization as string,
+    // );
+    const kakaoToken: any = req.headers.authorization;
     if (!kakaoToken) {
       throw new Error('KAKAO_TOKEN_ERROR');
     }
-
     const accessToken = await authService.kakaoSignin(kakaoToken);
     return res.status(200).json({ accessToken: accessToken });
   }
