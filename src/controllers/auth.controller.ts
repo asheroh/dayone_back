0cae832c315faa57a83037122073ee629a4304d7
SEONGJOON KIM <jobcho6320@naver.com>
2023-04-05 14:58:17 +0900

    feat: get kakao code

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -5,16 +5,22 @@ class auth {
   async kakaoLoginStart(req: Request, res: Response) {
     const redirectUrl = authService.getKakaoLoginUrl();
     console.log(redirectUrl);
-    // const redirect =
-    res.send(redirectUrl);
-    // const token = await this.kakaoRedirect(req, res);
-    // res.status(200).json({ JWT: token });
+    res.header('Access-Control-Allow-Origin', '*');
+    return res.redirect(redirectUrl);
+  }
+
+  async getKakakoAuthorize(req: Request, res: Response) {
+    const kakaoCode = req.query.code;
+    console.log(kakaoCode);
+
+    return kakaoCode;
   }
 
   async kakaoRedirect(req: Request, res: Response) {
-    // const kakaoAccessToken = await authService.getKakaoAccessToken(
-    //   req.headers.authorization as string,
-    // );
+    const kakaoAccessToken = await authService.getKakaoAccessToken(
+      req.headers.authorization as string,
+    );
+
     const kakaoToken: any = req.headers.authorization;
     if (!kakaoToken) {
       throw new Error('KAKAO_TOKEN_ERROR');
@@ -25,6 +31,8 @@ class auth {
 
   async getAllUsers(req: Request, res: Response) {
     const getAllUsers = authService.getAllUsers();
+    res.header('Access-Control-Allow-Origin', '*');
+
     res.json(getAllUsers);
   }
 }
